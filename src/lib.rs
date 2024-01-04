5bf1f0c85c304c9a7535487662ea6dca79f505b1
hzqd <hzqelf@yeah.net>
2024-01-04 14:11:55 +0800

    Simplified code

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,22 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{Data, Fields};
+use syn::{Data, Fields, DataStruct, Field, FieldsNamed};
 
 #[proc_macro_derive(Inherit)]
 pub fn derive_inherit(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
     let class = input.ident;
-    if let Data::Struct(s) = input.data {
-        if let Fields::Named(n) = s.fields {
-            if let Some(f) = n.named.into_iter().next() {
-                if let Some(i) = f.ident {
-                    let t = f.ty;
-                    return quote! {
-                        impl Deref for #class {
-                            type Target = #t;
-                            fn deref(&self) -> &Self::Target {
-                                &self.#i
-                            }
-                        }
-                    }.into()
+    if let Data::Struct(DataStruct { fields: Fields::Named(FieldsNamed { named, .. }), .. }) = input.data {
+        if let Some(Field { ident: Some(id), ty, .. }) = named.into_iter().next() {
+            return quote! {
+                impl Deref for #class {
+                    type Target = #ty;
+                    fn deref(&self) -> &Self::Target {
+                        &self.#id
+                    }
                 }
-            }
+            }.into()
         }
     }
-    quote!().into()
+    panic!("Inherit only support struct with named field")
 }
